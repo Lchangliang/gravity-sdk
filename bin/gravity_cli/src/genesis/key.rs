4c4ad7b66fbea747a66dadaef72ae14586a4cd30
Lightman <915311741@qq.com>
2025-08-20 11:24:05 +0800

    tmp

--- bin/gravity_cli/src/genesis/key.rs
+++ bin/gravity_cli/src/genesis/key.rs
@@ -1,13 +1,12 @@
 use clap::Parser;
 use gaptos::{
     api_types::u256_define::AccountAddress,
-    aptos_crypto::{bls12381, ed25519::{Ed25519PrivateKey, Ed25519PublicKey}, test_utils::KeyPair, x25519, PrivateKey, Uniform, ValidCryptoMaterial},
+    aptos_crypto::{ed25519::{self, Ed25519PublicKey}, PrivateKey, ValidCryptoMaterial},
     aptos_keygen::KeyGen,
     aptos_types::transaction::authenticator::AuthenticationKey,
 };
-
-use rand::thread_rng;
 use std::path::PathBuf;
+use tracing::info;
 use std::fs;
 
 use crate::command::Executable;
@@ -56,22 +55,17 @@ impl GenerateKey {
 impl Executable for GenerateKey {
     fn execute(self) -> Result<(), anyhow::Error> {
         println!("--- Generate Key Start ---");
-        let mut rng = thread_rng();
-        let network_private_key = x25519::PrivateKey::generate(&mut rng);
-        println!("The network_private_key is {:?}", network_private_key);
-        let consensus_private_key = bls12381::PrivateKey::generate(&mut rng);
-        println!("The consensus_private_key is {:?}", consensus_private_key);
-        println!("The consensus_public_key is {:?}", consensus_private_key.public_key().to_string());
+        let mut key_gen = self.key_generator()?;
+        let network_private_key = key_gen.generate_x25519_private_key()?;
+        let consensus_private_key = key_gen.generate_bls12381_private_key();
+        println!("The consensus_public_key is {:?}", consensus_private_key.public_key());
 
-        let account_kp = KeyPair::<Ed25519PrivateKey, Ed25519PublicKey>::generate(&mut rng);
-        println!("The account_private_key is {:?}", account_kp.private_key);
-        println!("The account_public_key is {:?}", account_kp.public_key);
-        // let account_address =
-        //     account_address_from_public_key(&account_kp.public_key);
-        // println!("The account_address is {:?}", account_address);
+        let account_private_key = key_gen.generate_ed25519_private_key();
+        let account_address = network_private_key.public_key();
+        println!("The account_address is {:?}", account_address);
         let indentity = ValidatorIndentity {
-            account_address: account_kp.public_key.to_string(),
-            account_private_key: hex::encode(account_kp.private_key.to_bytes()),
+            account_address: account_address.to_string(),
+            account_private_key: hex::encode(account_private_key.to_bytes()),
             consensus_private_key: hex::encode(consensus_private_key.to_bytes()),
             network_private_key: hex::encode(network_private_key.to_bytes()),
         };
@@ -84,11 +78,3 @@ impl Executable for GenerateKey {
     }
 }
 
-pub fn account_address_from_public_key(public_key: &Ed25519PublicKey) -> AccountAddress {
-    let auth_key = AuthenticationKey::ed25519(public_key);
-    account_address_from_auth_key(&auth_key)
-}
-
-pub fn account_address_from_auth_key(auth_key: &AuthenticationKey) -> AccountAddress {
-    AccountAddress::new(*auth_key.account_address())
-}
